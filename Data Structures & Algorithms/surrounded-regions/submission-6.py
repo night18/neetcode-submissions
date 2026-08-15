51f8da6dd27eabcdd12bf28ff9b4be2f12c25a9a
Chun-Wei Chiang <warrior.sp@gmail.com>
2026-08-15 14:58:24 -0700

    Add: surrounded-regions - submission-6

---
+++ Data Structures & Algorithms/surrounded-regions/submission-6.py
@@ -0,0 +1,35 @@
+from collections import deque
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "O":
+                    q = deque([(i, j)])
+                    visited = set()
+                    is_alive = False
+
+                    while q:
+                        x, y = q.popleft()
+                        if (x, y) in visited:
+                            continue
+                        visited.add((x, y))
+                        if x == 0 or x == len(board) - 1 or y == 0 or y == len(board[0]) - 1:
+                            is_alive = True
+                        
+                        for dx, dy in directions:
+                            if x + dx >= 0 and x + dx < len(board) and y + dy >= 0 and y + dy < len(board[0]) and board[x+dx][y+dy] == "O":
+                                q.append((x+dx, y+dy))
+
+                    if is_alive:
+                        for x, y in visited:
+                            board[x][y] = "T"
+                    else:
+                        for x, y in visited:
+                            board[x][y] = "X"
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "T":
+                    board[i][j] = "O"
